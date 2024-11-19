556bd543c08cd09e85fb8767f279874b3e4ecedf
Gourav NSS <gourav341111@gmail.com>
2024-11-19 11:29:57 +0530

    CHANGES ARE COMMITED

--- tryingpythopn.py
+++ tryingpythopn.py
@@ -1,3 +1,10 @@
-fruits = ("apple","banana","orange","cherry")
-for fruit in fruits:
-    print(fruits)
+n = list(map(int,input().split()))
+val = int(input())
+M = []
+#max element of list ee list lo una edho oka index ki less than or equal to avali
+#so index check cheyali ante while loop chedam
+index = len(n)
+for I in range(0,index):
+     if n[I] <= val:
+          M.append(n[I])
+print(max(M))
