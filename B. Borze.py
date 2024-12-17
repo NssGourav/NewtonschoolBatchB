9e23b571eadea2e6c883fda44be4ee9a63601ea8
NssGourav <gourav341111@gmail.com>
2024-12-17 10:11:55 +0530

    Add files via upload

---
+++ B. Borze.py
@@ -0,0 +1,16 @@
+a = list(input().strip())
+result = ""
+i = 0
+
+while i < len(a):
+    if a[i] == '.':
+        result += '0'
+        i += 1
+    elif a[i] == '-':
+        if a[i + 1] == '.':
+            result += '1'
+        else:
+            result += '2'
+        i += 2
+
+print(result)
