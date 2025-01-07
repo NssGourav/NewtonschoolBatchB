11e48e8a5e359c460934daf7909dbf9dfc76c3fc
NssGourav <gourav341111@gmail.com>
2025-01-07 11:29:40 +0530

    Create Swapping Two Numbers Using Only Two Variables.js

---
+++ Swapping Two Numbers Using Only Two Variables.js
@@ -0,0 +1,12 @@
+// Book categories
+let fiction = 120;
+let nonFiction = 80;
+let fantasy = 50;
+
+// Update book counts
+fiction += 20
+nonFiction += 10
+fantasy += 15
+
+// Calculate total books
+let totalBooks = fiction + nonFiction + fantasy
