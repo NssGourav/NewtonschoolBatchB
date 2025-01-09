f7aabbccfb7c58952d503083e85c2cd953f9a86d
Gourav N S S <gourav341111@gmail.com>
2025-01-09 11:29:53 +0530

    Create Bonus Challenge: String Operations.js

---
+++ Bonus Challenge: String Operations.js
@@ -0,0 +1,10 @@
+// str1 and str2 is already defined use them as it is 
+let concatenate = str1 + str2
+console.log( "Concatenated String:" +" "+concatenate)
+console.log("Length of String: "+ concatenate.length)
+if (str1 === str2){
+    console.log("Are strings equal?: true")
+}
+else{
+    console.log("Are strings equal?: false")
+}
