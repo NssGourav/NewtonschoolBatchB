82b98af07437dc92df810037e64f362f6114db1b
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-14 01:34:03 +0530

    Update Upper_lower_triangle.py

--- Upper_lower_triangle.py
+++ Upper_lower_triangle.py
@@ -1,1 +1,23 @@
 
+n=int(input())
+mat=[]
+for c in range (n):
+    lst=list(map(int,input().split()))
+    mat.append(lst)
+
+for i in range(n):
+    for j in range(n):
+        if(i>j):
+            print(0,end=" ")
+        else:
+            print(mat[i][j],end=" ")
+    print()
+print()
+
+for i in range(len(mat)):
+    for j in range(len(lst)):
+        if i<j:
+            print(0,end=" ")
+        else:
+            print(mat[i][j],end=" ")
+    print()
