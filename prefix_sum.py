2db1ea96183f569ce679fc02c8d66e60db8d67d7
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-16 23:05:41 +0530

    Update prefix_sum.py

--- prefix_sum.py
+++ prefix_sum.py
@@ -1,1 +1,16 @@
 
+n=int(input())
+lst=list(map(int,input().split()))
+q=int(input())
+
+prefix = [0]*n
+prefix[0]=lst[0]
+
+for i in range(1,n):
+    prefix[i]=prefix[i-1]+lst[i]
+for _ in range(q):
+    l,r=map(int,input().split())
+    if l==0:
+        print(prefix[r])
+    else:
+        print(prefix[r]-prefix[l-1])
