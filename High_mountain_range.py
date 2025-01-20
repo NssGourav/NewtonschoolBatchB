d93a57c19a7954d4071fcae689e8231ee924f1cb
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-20 19:28:50 +0530

    Update High_mountain_range.py

--- High_mountain_range.py
+++ High_mountain_range.py
@@ -1,1 +1,12 @@
 
+n = int(input()) 
+heights = list(map(int, input().split()))  
+
+max_jump = 0
+max_right = heights[-1]  
+for i in range(n - 2, -1, -1):
+    if max_right > heights[i]:  
+        max_jump = max(max_jump, max_right - heights[i])  
+    max_right = max(max_right, heights[i])  
+
+print(max_jump)
