818ffd39da28e60564f2b702e76ff355db37d6d6
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-20 19:28:02 +0530

    Update Little_alice(hard_version).py

--- Little_alice(hard_version).py
+++ Little_alice(hard_version).py
@@ -1,1 +1,17 @@
 
+n = int(input())
+arr = list(map(int, input().split()))
+
+pre_count=[0]*n
+pre_count[0]=(1 if arr[0]%5==0 else 0)
+
+for i in range(1,n):
+    pre_count[i]=pre_count[i-1] + (1 if arr[i]%5==0 else 0)
+
+q=int(input())
+for _ in range(q):
+    l,r=map(int,input().split())
+    if l==0:
+        print(pre_count[r]) 
+    else:
+        print(pre_count[r]-pre_count[l-1])
