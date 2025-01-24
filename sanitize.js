e860354dd02f4bb2c35a032b6ab518846da84a3c
Chanakyasinde <chanakyasinde@gmail.com>
2025-01-24 16:11:01 +0530

    Update sanitize.js

--- sanitize.js
+++ sanitize.js
@@ -1,1 +1,11 @@
-
+return function sanitizeInput(input) {
+      let s= ""
+      for (let i=0; i<input.length; i++) {
+            if (input[i]=="@"||input[i]=="#"||input[i]=="$"||input[i]=="%"||input[i]=="&"||input[i]=="*"||input[i]=="!"||input[i]=="<"||input[i]==">"||input[i]=="?") {
+                  s+="_"
+            } else {
+                  s+=input[i]
+            }
+      }
+      return s
+}
