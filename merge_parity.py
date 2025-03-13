bca41af70bfe274643773d53207a1967407e38b0
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-13 22:27:43 +0530

    Update merge_parity.py

--- merge_parity.py
+++ merge_parity.py
@@ -43,3 +43,34 @@ ans.extend(merge(even1,even2))
 ans.extend(merge(odd1,odd2))
 print(*ans)
 
+
+
+# def merge_parity(n, m, arr, arr2):
+#     merged = []
+#     p1=0
+#     p2=0
+#     while p1 < n and p2 < m:
+#         if arr[p1] < arr2[p2]:
+#             merged.append(arr[p1])
+#             p1 += 1
+#         else:
+#             merged.append(arr2[p2])
+#             p2 += 1  
+#     while p1 < n:
+#         merged.append(arr[p1])
+#         p1 += 1
+#     while p2 < m:
+#         merged.append(arr2[p2])
+#         p2 += 1
+#     even = []
+#     odd = []
+#     for ele in merged:
+#         if ele%2==0:
+#             even.append(ele)
+#         else:
+#             odd.append(ele)
+#     print(*even,*odd)
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# arr2= list(map(int, input().split()))
+# merge_parity(n, m, arr,arr2)
