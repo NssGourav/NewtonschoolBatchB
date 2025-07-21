39a0f51e312c56e6e0a6717baa2cc5416044d11d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-21 15:39:24 +0530

    Update hannoi_tower.py

--- hannoi_tower.py
+++ hannoi_tower.py
@@ -1,1 +1,15 @@
+rods = {'A', 'B', 'C'}
 
+def move(n, source, destination):
+    if n==0:
+        return 
+    
+    current = {source, destination}
+
+    extra = (rods - current).pop()
+
+    move(n-1, source, extra)
+    print(f"Move disk {n} from {source} to {destination}")
+    move(n-1, extra, destination)
+
+move(3, "A", "B")
