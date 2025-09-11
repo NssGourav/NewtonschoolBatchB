d11999fc4b482fd67c14976fa055e83dbacc3f80
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-11 22:23:56 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,1 +1,24 @@
+def bfsOfGraph(n, edges, src):
+    adj = [[] for _ in range(n)]
+    
+    for u,v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    visited = [False]*n
+    q = deque()
+    bfs = []
 
+    q.append(src)
+    visited[src] = True
+
+    while len(q) > 0:
+        val = q.popleft()
+        bfs.append(val)
+
+        for neighbour in adj[val]:
+            if visited[neighbour] == False:
+                q.append(neighbour)
+                visited[neighbour] = True
+    
+    return bfs
