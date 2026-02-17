2d298bf84eb51863e58249a45d53eeaed90b6d12
Chanakya Sinde <chanakyasinde@gmail.com>
2026-02-17 22:17:19 +0530

    Remove functionn from A. YES or YES?.py

--- A. YES or YES?.py
+++ A. YES or YES?.py
@@ -1,9 +1,1 @@
-def functionn(a):
-    results = []
-    for i in range(a):
-        k = input().strip()  
-        if k.lower() == "yes":  
-            results.append("YES")
-        else:
-            results.append("NO")
-    return ('\n'.join(results))
+
