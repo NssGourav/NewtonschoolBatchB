68b6416ed5d4ab037c7447551d0680a31c7d3584
Chanakya Sinde <chanakyasinde@gmail.com>
2026-02-20 22:35:49 +0530

    Remove Function definition from Array Indexing.py

--- Array Indexing.py
+++ Array Indexing.py
@@ -1,11 +1,1 @@
-def Function(N, A):
-    total_sum = sum(A)
-    left_sum = 0
-    count = 0
-    for i in range(N):
-        right_sum = total_sum - left_sum - A[i]
-        if left_sum >= right_sum:
-            count += 1
-        left_sum += A[i]
 
-    return count
