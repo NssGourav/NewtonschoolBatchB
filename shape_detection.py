0e6c767b0fe86a2c5ae66124ea64c03de35a4db9
Chanakya Sinde <chanakyasinde@gmail.com>
2026-04-05 22:48:41 +0530

    Update shape_detection.py

--- shape_detection.py
+++ shape_detection.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def detect_distribution_shape(df: pd.DataFrame) -> str:
+    skewness = df["PurchaseAmount"].skew()
+    
+    if skewness > 0.5:
+        return "Right Skewed"
+    elif skewness < -0.5:
+        return "Left Skewed"
+    else:
+        return "Symmetric"
